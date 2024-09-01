e809d0a78f0cbd66e0ce57487e5503bde2fd24dd
JaiDhiman <jaidhiman2000@gmail.com>
2024-09-01 10:45:38 -0700

    added jbuilder

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,11 @@
 class ContactsController < ApplicationController
+  def all
+    @contacts = Contact.all
+    render template: "contacts/index"
+  end
+
   def first_contact
-    contact = Contact.find_by(id: 1)
-    render json: { contact: contact }
+    @contact = Contact.find_by(id: 1)
+    render template: "contacts/show"
   end
 end
